095cb5e2a277cc99b1b958aa1e30c9f9d50e93a9
Lea Kapelevich <leakapelevich@gmail.com>
2021-07-29 10:56:39 +0200

    make a reference runner

--- examples/CBLIB/JuMP_test.jl
+++ examples/CBLIB/JuMP_test.jl
@@ -1,21 +1,21 @@
 
 # directory of CBLIB files
-cblib_dir = joinpath(ENV["HOME"], "cblib/cblib.zib.de/download/all")
+cblib_dir = "C:\\Users\\leaka\\cblib\\cblib.zib.de\\download\\all"
 if !isdir(cblib_dir)
     @warn("CBLIB download folder not found")
     cblib_dir = joinpath(@__DIR__, "cblib_data")
     cblib_diverse = String[]
 else
     cblib_diverse = [
-        "expdesign_D_8_4", # psd, exp
-        "port_12_9_3_a_1", # psd, soc, exp
-        "tls4", # soc
-        "ck_n25_m10_o1_1", # rsoc
+        # "expdesign_D_8_4", # psd, exp
+        # "port_12_9_3_a_1", # psd, soc, exp
+        # "tls4", # soc
+        # "ck_n25_m10_o1_1", # rsoc
         "rsyn0805h", # exp
-        "2x3_3bars", # psd
-        "HMCR-n20-m400", # power
-        "classical_20_0", # soc, orthant
-        "achtziger_stolpe06-6.1flowc", # rsoc, orthant
+        # "2x3_3bars", # psd
+        # "HMCR-n20-m400", # power
+        # "classical_20_0", # soc, orthant
+        # "achtziger_stolpe06-6.1flowc", # rsoc, orthant
         "LogExpCR-n100-m400", # exp, orthant
         ]
 end
